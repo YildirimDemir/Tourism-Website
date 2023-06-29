14e63ed1f87c15c267428fd86c3c97f11a79dd7f
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-06-29 11:54:24 +0300

    transport

---
+++ transport.js
@@ -0,0 +1,24 @@
+document.getElementById("b1").addEventListener("click", function() {
+
+  var satirElementleri = document.getElementsByClassName("plane");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+document.getElementById("b2").addEventListener("click", function() {
+
+  var satirElementleri = document.getElementsByClassName("ship");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+
+document.getElementById("b3").addEventListener("click", function() {
+
+  var satirElementleri = document.getElementsByClassName("bus");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+
+document.getElementById("b4").addEventListener("click", function() {
+
+  var satirElementleri = document.getElementsByClassName("train");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+
+  
