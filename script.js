3a09ca9840e71521b5555edbd39670f24ec9d4a0
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-07-02 16:17:06 +0300

    sonlandırdım

--- script.js
+++ script.js
@@ -81,3 +81,47 @@ window.addEventListener('DOMContentLoaded', function () {
 });
 
 //******************************************** OTEL-DETAY-END **********************************************************/
+//! Transport-Start
+document.getElementById("b1").addEventListener("click", function() {
+
+  let satirElementleri = document.getElementsByClassName("plane");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+document.getElementById("b2").addEventListener("click", function() {
+
+  let satirElementleri = document.getElementsByClassName("ship");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+
+document.getElementById("b3").addEventListener("click", function() {
+
+  let satirElementleri = document.getElementsByClassName("bus");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+
+document.getElementById("b4").addEventListener("click", function() {
+
+  let satirElementleri = document.getElementsByClassName("train");
+    satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+});
+let butons = document.getElementsByClassName("butn");
+
+for (let i=0; i < butons.length; i++ ) {
+  butons[i].addEventListener("click", function() {
+  
+    let satirElementleri = document.getElementsByClassName("login");
+      satirElementleri[0].scrollIntoView({ behavior: "auto", block:"center" });
+  });
+}
+//! Transport-End
+
+
+//! Tickets-Start
+function showSelectedText(selectId) {
+  let selectElement = document.getElementById("country" + selectId);
+  let selectedOption = selectElement.options[selectElement.selectedIndex];
+  let selectedText = selectedOption.value;
+  let resultElement = document.getElementById("prc" + selectId);
+  resultElement.textContent = selectedText;
+}
+//! Tickets-End
--- tickets.js
+++
@@ -1,7 +0,0 @@
-function showSelectedText(selectId) {
-    let selectElement = document.getElementById("country" + selectId);
-    let selectedOption = selectElement.options[selectElement.selectedIndex];
-    let selectedText = selectedOption.value;
-    let resultElement = document.getElementById("prc" + selectId);
-    resultElement.textContent = selectedText;
-  }
